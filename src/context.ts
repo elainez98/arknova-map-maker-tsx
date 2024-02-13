21a93724c844df1b42b64053a21db0faf3d423d4
Kevin <TangerineCat@users.noreply.github.com>
2024-02-12 22:47:46 -0800

    Add support for noting upgraded build required tiles

--- src/context.ts
+++ src/context.ts
@@ -1,12 +1,13 @@
-import { createContext } from 'react';
-import { Terrain } from './Model/terrain';
 import { Icon } from './Model/icon';
+import { Terrain } from './Model/terrain';
+import { createContext } from 'react';
 
 export interface BrushSelection {
   icon?: Icon;
   terrain?: Terrain
   value?: number
   deleteIcon?: boolean
+  buildUpgrade?: boolean
 }
 
 export const BrushSelectionContext = createContext<BrushSelection>({});
